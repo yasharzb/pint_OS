28f3ef71aa870ceeaaf08bc74a4939315a55eca0
yasharzb <yasharzb@chmail.ir>
2021-05-25 21:20:48 +0000

    full-bw -> test 2

--- pintos/src/tests/filesys/extended/full-bw.c
+++ pintos/src/tests/filesys/extended/full-bw.c
@@ -6,18 +6,25 @@
 
 void test_main(void)
 {
-    int fd = open("sample_1hk.txt");
-    int read_cnt = blk_read_cnt();
-    int wr_cnt = blk_wr_cnt();
+    int fd = open("sample_64k.txt");
     int bytes_written = 0;
+    int bytes_read = 0;
     char *buf[BLOCK_SECTOR_SIZE];
     int expected_wr_cnt = 0;
-    // memset(buf, 'a', BLOCK_SECTOR_SIZE * 1 );
-    memset(buf, 'a', sizeof buf);
-    for (int i = 0; (bytes_written = write(fd, buf, BLOCK_SECTOR_SIZE)) > 0; i++){
+    int expected_read_cnt = 0;
+    memset(buf, 'a', 1);
+    // memset(buf, 'a', sizeof buf);
+    for (int i = 0; (bytes_written = write(fd, buf, 1)) > 0 && expected_wr_cnt < 64*1024; i++)
+    {
         expected_wr_cnt++;
     }
+    seek(fd, 0);
+    int wr_cnt = blk_wr_cnt();
+    for (int i = 0; (bytes_read = read(fd, buf, BLOCK_SECTOR_SIZE)) > 0; i++)
+    {
+        expected_read_cnt++;
+    }
     close(fd);
-    CHECK((blk_read_cnt() - read_cnt) == 0, "block_read operation didn't happen");
+    msg("%d %d", expected_wr_cnt, blk_wr_cnt() - wr_cnt);
     CHECK((blk_wr_cnt() - wr_cnt) == expected_wr_cnt, "block_write operation invokation count matches the exception");
 }
