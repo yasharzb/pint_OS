c90aea69194ac1d9a97203ae92ac5cc9a608d274
yasharzb <yasharzb@chmail.ir>
2021-05-25 21:58:33 +0000

    Fix full-bw

--- pintos/src/tests/filesys/extended/full-bw.c
+++ pintos/src/tests/filesys/extended/full-bw.c
@@ -6,7 +6,9 @@
 
 void test_main(void)
 {
-    int fd = open("sample_64k.txt");
+    int init_size = 64 * 1024;
+    create("samp_64k.txt", init_size);
+    int fd = open("samp_64k.txt");
     int bytes_written = 0;
     int bytes_read = 0;
     char *buf[BLOCK_SECTOR_SIZE];
@@ -14,17 +16,18 @@ void test_main(void)
     int expected_read_cnt = 0;
     memset(buf, 'a', 1);
     // memset(buf, 'a', sizeof buf);
-    for (int i = 0; (bytes_written = write(fd, buf, 1)) > 0 && expected_wr_cnt < 64*1024; i++)
+    for (int i = 0; (bytes_written = write(fd, buf, 1)) > 0 && expected_wr_cnt < init_size; i++)
     {
         expected_wr_cnt++;
     }
     seek(fd, 0);
     int wr_cnt = blk_wr_cnt();
-    for (int i = 0; (bytes_read = read(fd, buf, BLOCK_SECTOR_SIZE)) > 0; i++)
+    for (int i = 0; (bytes_read = read(fd, buf, 1)) > 0; i++)
     {
         expected_read_cnt++;
     }
     close(fd);
-    msg("%d %d", expected_wr_cnt, blk_wr_cnt() - wr_cnt);
-    CHECK((blk_wr_cnt() - wr_cnt) == expected_wr_cnt, "block_write operation invokation count matches the exception");
+    remove("samp_64k.txt");
+    int dif = (blk_wr_cnt() - wr_cnt) ;
+    CHECK(dif > 2 * init_size / 1024 && dif < 3 * init_size / 1024, "block_write operation invokation count matches the exception");
 }
--- pintos/src/tests/filesys/extended/full-bw.ck
+++ pintos/src/tests/filesys/extended/full-bw.ck
@@ -4,7 +4,6 @@ use warnings;
 use tests::tests;
 check_expected ([<<'EOF']);
 (full-bw) begin
-(full-bw) block_read operation didn't happen
 (full-bw) block_write operation invokation count matches the exception
 (full-bw) end
 full-bw: exit(0)
